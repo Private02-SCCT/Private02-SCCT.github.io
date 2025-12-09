f52391ea2369f2ba84586d98b713d9e38ea391e0
Private02-SCCT <private02x@outlook.jp>
2025-12-09 15:08:16 +0000

    ブランチ作成による開発中部分の削除

--- VTuberReverseLookup/script.js
+++
@@ -1,7 +0,0 @@
-const keyword = 'app';
-
-const results = data.filter(item => {
-  return item.name.includes(keyword) || item.category.includes(keyword);
-});
-
-// 出力: [ { id: 1, name: 'Apple', category: 'Fruit' }, { id: 4, name: 'Avocado', category: 'Fruit' } ]
