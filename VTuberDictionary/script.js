f9c5dc40afe0925471acc7317392110389f63909
Private02-SCCT <private02x@outlook.jp>
2025-12-10 17:09:41 +0000

    tmp push

--- VTuberDictionary/script.js
+++ VTuberDictionary/script.js
@@ -40,24 +40,49 @@ function handleSearch() {
     const searchResults = fuseInstance.search(keyword);
 
     if (searchResults.length > 0) {
-        const ul = document.createElement('ul');
         searchResults.forEach(result => {
-            const li = document.createElement('li');
             const name = result.item.name;
             const id = result.refIndex;
-
-            li.textContent = `[ID: ${id}] ${name} (スコア: ${result.score.toFixed(3)})`;
-            ul.appendChild(li);
+            `[ID: ${id}] ${name} (スコア: ${result.score.toFixed(3)})`;
         });
-        resultsDiv.appendChild(ul);
     } else {
         resultsDiv.textContent = `キーワード「${keyword}」に一致する結果は見つかりませんでした。`;
     }
 }
 
-const resultsDiv = document.getElementById("resultDiv")
+
+// [
+//   // 1番目の検索結果（スコアが最も低い＝最も一致度が高い）
+//   {
+//     "item": { /* 元のデータ配列から抽出された、一致したオブジェクト全体 */
+//       "name": "大空スバル",
+//       "furigana": "OozoraSubaru",
+//       "production": "ホロライブ",
+//       // ... その他のプロパティ ...
+//     },
+//     "refIndex": 0, // ★ 元の data 配列でのインデックス（ID）
+//     "score": 0.125, // ★ 検索の一致度スコア (0.000が完全一致)
+//     "matches": [ /* どのプロパティで一致したかの詳細情報（optionsで有効化した場合） */
+//       {
+//         "key": "production",
+//         "value": "ホロライブ",
+//         "indices": [
+//           [0, 1] // キーワードとデータが一致した文字列の位置
+//         ]
+//       }
+//     ]
+//   },
+
+const resultsDiv = document.getElementById("status")
 const searchInput = document.getElementById("search")
 const btn = document.getElementById("btn")
 btn.addEventListener('click', handleSearch);
 
 initializeFuse();
+
+new gridjs.Grid({
+        columns: ["文字列","数値"],
+        data: [["高級車",5000000],
+                ["住宅",30000000],
+                ["自転車",30000]]
+}).render(document.getElementById("gridTag"));
