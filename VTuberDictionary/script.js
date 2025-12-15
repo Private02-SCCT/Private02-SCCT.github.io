82dfa195c2eecdd4df8723e90ecaf5bcdae8888e
Private02-SCCT <private02x@outlook.jp>
2025-12-15 15:33:46 +0000

    updated

--- VTuberDictionary/script.js
+++ VTuberDictionary/script.js
@@ -138,9 +138,27 @@ function handleSearch() {
     }
 }
 
+function handleScroll() {
+  const header = document.querySelector(".headerBox");
+
+  const scrollThreshold = 100;
+
+  if (window.scrollY > scrollThreshold) {
+    if (!header.classList.contains("scrolled")) {
+      header.classList.add("scrolled");
+    }
+  } else {
+    if (header.classList.contains("scrolled")) {
+      header.classList.remove("scrolled");
+    }
+  }
+}
+
 const resultsDiv = document.getElementById("status")
 const searchInput = document.getElementById("search")
 const btn = document.getElementById("btn")
+
+window.addEventListener("scroll", handleScroll);
 btn.addEventListener('click', handleSearch);
 
 initializeFuse();
